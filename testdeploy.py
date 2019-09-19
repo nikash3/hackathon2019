59540bb2baf629cf913f647870a04a6ce2d82aac
nikashkumar3 <nikashkumar03@gmail.com>
2019-09-19 22:20:53 +0530

    test deploy py

--- testdeploy.py
+++ testdeploy.py
@@ -3,8 +3,5 @@ from flask import Flask
 app = Flask(__name__) #creating the Flask class object   
  
 @app.route('/') #decorator drfines the   
-def home():  
+def index():  
     return "hello, this is our first flask website";  
-  
-if __name__ =='__main__':  
-    app.run(debug = True)  
