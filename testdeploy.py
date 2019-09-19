d519bb07b3f5f88adaa327e27c2f6c906a137df8
nikashkumar3 <nikashkumar03@gmail.com>
2019-09-19 22:16:00 +0530

    heroku setup

---
+++ testdeploy.py
@@ -0,0 +1,10 @@
+from flask import Flask  
+  
+app = Flask(__name__) #creating the Flask class object   
+ 
+@app.route('/') #decorator drfines the   
+def home():  
+    return "hello, this is our first flask website";  
+  
+if __name__ =='__main__':  
+    app.run(debug = True)  
